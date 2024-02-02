8a59ca2e6688e3d7f0d3a2652e130e5f156c1c1a
IvanDirigible <sickeruncible@gmail.com>
2024-02-02 16:16:29 -0500

    Added seed util.

---
+++ utils/data.js
@@ -0,0 +1,117 @@
+const users = [
+    {
+      "username": "wizardguy",
+      "email": "wizguy@hotmail.com",
+      "thoughts": ["Barbenheimer was a triumph!", "Tried that new Indian restaurant on 5th. It was great!"],
+      "friends": [2,6]
+    },
+    {
+      "username": "longcat777",
+      "email": "looooooooongcat@gmail.com",
+      "thoughts": ["Cats are the pinnacle of evolution."],
+      "friends": [1,6]
+    },
+    {
+      "username": "TacoTuesday",
+      "email": "tacotues@gmail.com",
+      "thoughts": ["Doing what I do every night: Make tacos.", "Making tacos again.", "Tacos once more."],
+      "friends": [1,2,4,6]
+    },
+    {
+      "username": "TheRealBob",
+      "email": "fakebob@msn.com",
+      "thoughts": ["I'm not even a real person!"],
+      "friends": [2,3]
+    },
+    {
+      "username": "person4weather",
+      "email": "weatherperson@hotmail.com",
+      "thoughts": ["Bring your umbrellas! It's going to be another rainy weekend."],
+      "friends": [1]
+    },
+    {
+      "username": "keyboardconnoisseur",
+      "email": "thekeyboardist@gmail.com",
+      "thoughts": ["Just got my Logitech MX Mechanical. I still prefer the SteelSeries Apex Pro."],
+      "friends": [3]
+    },
+    {
+      "username": "xX7331GamerXx",
+      "email": "07d$ch007@aol.com",
+      "thoughts": ["Did you know? You swallow a pound of game controllers in your lifetime?", "Did you know? There's more gold in a pound of controllers than there is in a pound of gold?"],
+      "friends": [1]
+    },
+  ];
+  
+  const thoughts = [
+    {
+      "thoughtText": ["Barbenheimer was a triumph!"],
+      "createdAt": "",
+      "username": "wizardguy",
+      "reactions": []
+    },
+    {
+      "thoughtText": ["Tried that new Indian restaurant on 5th. It was great!"],
+      "createdAt": "",
+      "username": "wizardguy",
+      "reactions": []
+    },
+    {
+      "thoughtText": ["Cats are the pinnacle of evolution."],
+      "createdAt": "",
+      "username": "longcat777",
+      "reactions": []
+    },
+    {
+      "thoughtText": ["Doing what I do every night: Make tacos."],
+      "createdAt": "",
+      "username": "TacoTuesday",
+      "reactions": []
+    },
+    {
+      "thoughtText": ["Making tacos again."],
+      "createdAt": "",
+      "username": "TacoTuesday",
+      "reactions": []
+    },
+    {
+      "thoughtText": ["Tacos once more. As is tradition."],
+      "createdAt": "",
+      "username": "TacoTuesday",
+      "reactions": []
+    },
+    {
+      "thoughtText": ["I'm not even a real person!"],
+      "createdAt": "",
+      "username": "TheRealBob",
+      "reactions": []
+    },
+    {
+      "thoughtText": ["Bring your umbrellas! It's going to be another rainy weekend."],
+      "createdAt": "",
+      "username": "person4weather",
+      "reactions": []
+    },
+    {
+      "thoughtText": ["Just got my Logitech MX Mechanical. I still prefer the SteelSeries Apex Pro."],
+      "createdAt": "",
+      "username": "keyboardconnoisseur",
+      "reactions": []
+    },
+    {
+      "thoughtText": ["Did you know? You swallow a pound of game controllers in your lifetime?"],
+      "createdAt": "",
+      "username": "xX7331GamerXx",
+      "reactions": []
+    },
+    {
+      "thoughtText": ["Did you know? There's more gold in a pound of controllers than there is in a pound of gold?"],
+      "createdAt": "",
+      "username": "xX7331GamerXx",
+      "reactions": []
+    },
+  ];
+
+  const reactions = [
+
+  ];
